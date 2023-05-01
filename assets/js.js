3ad568b5300cbe437a3fbe091396150f78a0781f
goyo2205 <jgregorio.ef@gmail.com>
2023-05-01 12:09:16 -0600

    mejoro falta reset

--- assets/js.js
+++ assets/js.js
@@ -3,15 +3,13 @@ let guess = 50;
 let max = 100;
 let min = 1;
 
-function mostrar(){
-    document.getElementById('si').style.display = 'inline-block';
-    document.getElementById('no').style.display = 'inline-block';
-    document.getElementById('ese').style.display = 'inline-block';
+function mostrar(ref){
+    console.log(ref)
+    document.getElementById(ref).style.display = 'inline';
+
 };
-function ocultar(){
-    document.getElementById('si').style.display = 'none';
-    document.getElementById('no').style.display = 'none';
-    document.getElementById('ese').style.display = 'none';
+function ocultar(ref){
+    document.getElementById(ref).style.display = 'none';
 };
 function deshabilitar(refObj){
     console.log(`"${document.getElementById(refObj.id).id}" button has been disabled`);
@@ -21,20 +19,34 @@ function deshabilitar(refObj){
 function preguntar(){
     document.getElementById('pregunta').innerText = `Tu número es el ${guess}?`;
 };
+
+terminator:
 function generar(validar){
     if(validar == 'si'){
         console.log('aumentar');
+        console.log(max);
         min = guess;
         guess = (max + guess)/2;
         guess = Math.trunc(guess);
-        if (guess == max) encontrado();
+        console.log(min);
+        console.log(guess);
+        if (guess == min){
+            guess++;
+            encontrado();
+            return;
+        }
     }
     else if(validar == 'no'){
         console.log('disminuir');
         max = guess;
         guess = (min + guess)/2;
         guess = Math.trunc(guess);
-        if (guess == min) encontrado();
+        console.log(max);
+        console.log(guess);
+        if (guess == 1) {
+            encontrado();
+            return;
+        }
     }
     guess = Math.trunc(guess)
     preguntar();
@@ -42,5 +54,11 @@ function generar(validar){
 function encontrado(){
     console.log('fin');
     document.getElementById('pregunta').innerText = `Tu número es el ${guess}, excelente decisión`;
-    ocultar();
+    ocultar('botones');
+    mostrar('restart');
+}
+function restart(){
+    guess = 50;
+    max = 100;
+    min = 1;
 }
